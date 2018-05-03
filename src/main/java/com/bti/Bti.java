531b2db156de7396fb22f091d70e68b316f74d4e
PriyankaMagadum <34264310+PriyankaMagadum@users.noreply.github.com>
2018-05-03 13:23:46 +0530

    Update Bti.java

--- src/main/java/com/bti/Bti.java
+++ src/main/java/com/bti/Bti.java
@@ -17,6 +17,7 @@ import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
 public class Bti {
 
 	public static void main(String[] args) {
+		System.out.println("Helloooo");
 		SpringApplication.run(Bti.class, args);
 	}
 }
